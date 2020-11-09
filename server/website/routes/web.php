5895313fd2074daca38a4fcae2132de67eb7b053
Frendy <frndy@hawaii.edu>
2020-11-08 17:00:59 -1000

    Added a dummy controller for reference

--- server/website/routes/web.php
+++ server/website/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [TestController::class, 'getTestInfo']);
