2dbc709696e36bd1758b2b387307fa9d56aa5b82
spjy <spjy@hawaii.edu>
2020-11-08 18:27:07 -1000

    feat: create navbar

--- ui/components/Global/Navbar.jsx
+++ ui/components/Global/Navbar.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import Link from 'next/link';
 
 function Navbar() {
   return (
-    <div className="flex">
-      <div>GameTheSystem</div>
+    <div className="flex justify-between text-white bg-color p-4">
+      <div className="flex">
+        <div className="font-bold">
+          <Link href="/">GameTheSystem</Link>
+        </div>
+        <div className="pl-8">
+          <Link href="/schedule">Schedule</Link>
+        </div>
+        <div className="pl-8">
+          <Link href="/dashboard">Dashboard</Link>
+        </div>
+      </div>
+      <div className="flex">
+        <div>
+          <Link href="/appointments">
+            My appointments
+          </Link>
+        </div>
+        <div className="pl-8">
+          <Link href="/schedule">Login</Link>
+        </div>
+      </div>
     </div>
   );
 }
