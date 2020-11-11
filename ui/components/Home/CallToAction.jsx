3dca2ff4661e0ebf9dcf5aaab8aacb4cf753cc67
ilee72 <46728285+ilee72@users.noreply.github.com>
2020-11-10 23:50:15 -1000

    landing page complete?

--- ui/components/Home/CallToAction.jsx
+++ ui/components/Home/CallToAction.jsx
@@ -1,8 +1,331 @@
 import React from 'react';
+import Link from 'next/link';
 
 function CallToAction() {
   return (
-    <div className="flex">
+      <div className="py-12 bg-white">
+        <div className="relative bg-white overflow-hidden">
+          <div className="max-w-screen-xl mx-auto">
+            <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+              <svg
+                  className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+                  fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <polygon points="50,0 100,0 50,100 0,100"/>
+              </svg>
+
+              <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
+                  <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                  </div>
+                </nav>
+              </div>
+
+              <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+                <div className="rounded-lg shadow-md">
+                  <div className="rounded-lg bg-white shadow-xs overflow-hidden" role="menu" aria-orientation="vertical"
+                       aria-labelledby="main-menu">
+                  </div>
+                </div>
+              </div>
+
+              <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+                <div className="sm:text-center lg:text-left">
+                  <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+                    Tired of using Google Spreadsheets?
+                  </h2>
+                  <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                    You can now automate the process of creating appointments, checking in, and track location history
+                    with our QR code based system!
+                  </p>
+                  <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                    <div className="rounded-md shadow">
+                      <a href="/schedule"
+                         className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-800 hover:bg-blue-200 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                        Get Started
+                      </a>
+                    </div>
+                    <div className="mt-3 sm:mt-0 sm:ml-3">
+                      <a href="/login"
+                         className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-blue-700 bg-blue-100 hover:text-blue-700 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                        Login Here
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </main>
+            </div>
+          </div>
+          <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+                 src="https://cdnassets.hw.net/dims4/GG/f430612/2147483647/resize/850x%3E/quality/90/?url=https%3A%2F%2Fcdnassets.hw.net%2Fe0%2Fd7%2Fcf344d0d4fff98bd28c292366ade%2F166f9932-fa9d-45d9-b50c-301d1bc41029.jpg"
+                 alt=""/>
+          </div>
+        </div>
+        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-center">
+            <h3 className="mt-2 text-3xl leading-6 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+              Information Technology Services
+            </h3>
+            <p className="mt-6 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+              Our mission is to provide the best user interface for the management of access control across the entire campus!
+            </p>
+          </div>
+
+          <div className="mt-10">
+            <ul className="md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+              <li>
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-200 text-blue-800">
+
+                      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v1m6 11h2m-6 0h-2v4m0-11v3m0 0h.01M12 12h4.01M16 20h4M4 12h4m12 0h.01M5 8h2a1 1 0 001-1V5a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1zm12 0h2a1 1 0 001-1V5a1 1 0 00-1-1h-2a1 1 0 00-1 1v2a1 1 0 001 1zM5 20h2a1 1 0 001-1v-2a1 1 0 00-1-1H5a1 1 0 00-1 1v2a1 1 0 001 1z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h4 className="text-lg leading-6 font-medium text-gray-900">QR Codes</h4>
+                    <p className="mt-2 text-base leading-6 text-gray-500">
+                      Check in guests and track their location history with your mobile devices.
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="mt-10 md:mt-0">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-200 text-blue-800">
+
+                      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h4 className="text-lg leading-6 font-medium text-gray-900">Appointments</h4>
+                    <p className="mt-2 text-base leading-6 text-gray-500">
+                      Create and send appointments via email and keep track of appointment times and dates.
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="mt-10 md:mt-0">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-200 text-blue-800">
+
+                      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h4 className="text-lg leading-6 font-medium text-gray-900">Verification</h4>
+                    <p className="mt-2 text-base leading-6 text-gray-500">
+                      Use photo verification to allow of secure check ins and added levels of security within facilities.
+                    </p>
+                  </div>
+                </div>
+              </li>
+              <li className="mt-10 md:mt-0">
+                <div className="flex">
+                  <div className="flex-shrink-0">
+                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-blue-200 text-blue-800">
+
+                      <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                      </svg>
+                    </div>
+                  </div>
+                  <div className="ml-4">
+                    <h4 className="text-lg leading-6 font-medium text-gray-900">Security</h4>
+                    <p className="mt-2 text-base leading-6 text-gray-500">
+                      Our web application will lower personnel costs and improve security and information management.
+                    </p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default CallToAction;
+
+/*<div className="relative bg-white overflow-hidden">
+  <div className="max-w-screen-xl mx-auto">
+    <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+      <svg className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+           fill="currentColor" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <polygon points="50,0 100,0 50,100 0,100"/>
+      </svg>
+
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start">
+          <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+          </div>
+        </nav>
+      </div>
+
+      <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+        <div className="rounded-lg shadow-md">
+          <div className="rounded-lg bg-white shadow-xs overflow-hidden" role="menu" aria-orientation="vertical"
+               aria-labelledby="main-menu">
+          </div>
+        </div>
+      </div>
+
+      <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div className="sm:text-center lg:text-left">
+          <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+            Tired of using Google Spreadsheets?
+          </h2>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            You can now automate the process of creating appointments, checking in, and track location history with our QR code based system!
+          </p>
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <a href="/schedule"
+                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-800 hover:bg-blue-200 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                Get Started
+              </a>
+            </div>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <a href="/login"
+                 className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-blue-700 bg-blue-100 hover:text-blue-700 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+                Login Here
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+  <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+    <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+         src="https://cdnassets.hw.net/dims4/GG/f430612/2147483647/resize/850x%3E/quality/90/?url=https%3A%2F%2Fcdnassets.hw.net%2Fe0%2Fd7%2Fcf344d0d4fff98bd28c292366ade%2F166f9932-fa9d-45d9-b50c-301d1bc41029.jpg"
+         alt=""/>
+  </div>
+</div>*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*<div class="py-12 bg-white">
+  <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+
+    <div class="lg:text-center">
+      <div class="lg:center">
+      <img src="/confirmation.svg" alt="Confirmation" />
+      </div>
+      <p class="text-base mt-5 leading-6 text-blue-300 font-semibold tracking-wide uppercase">GameTheSystem</p>
+      <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+        Tired of using Google Spreadsheets?
+      </h3>
+      <p class="mt-8 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+        With our web application, you can now automate the creation of appointments.
+      </p>
+    </div>
+
+    <div class="mt-10">
+      <ul class="md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+        <li>
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                </svg>
+              </div>
+            </div>
+            <div class="ml-4">
+              <h4 class="text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</h4>
+              <p class="mt-2 text-base leading-6 text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+        </li>
+        <li class="mt-10 md:mt-0">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                </svg>
+              </div>
+            </div>
+            <div class="ml-4">
+              <h4 class="text-lg leading-6 font-medium text-gray-900">No hidden fees</h4>
+              <p class="mt-2 text-base leading-6 text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+        </li>
+        <li class="mt-10 md:mt-0">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                </svg>
+              </div>
+            </div>
+            <div class="ml-4">
+              <h4 class="text-lg leading-6 font-medium text-gray-900">Transfers are instant</h4>
+              <p class="mt-2 text-base leading-6 text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+        </li>
+        <li class="mt-10 md:mt-0">
+          <div class="flex">
+            <div class="flex-shrink-0">
+              <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+
+                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                </svg>
+              </div>
+            </div>
+            <div class="ml-4">
+              <h4 class="text-lg leading-6 font-medium text-gray-900">Mobile notifications</h4>
+              <p class="mt-2 text-base leading-6 text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>*/
+
+
+
+
+/*    <div className="flex">
+      <div className="w-1/2">
+        <img src="/confirmation.svg" alt="Confirmation" />
+      </div>
       <div className="grid w-1/2 place-content-center">
         <h1 className="text-3xl font-bold mb-4">
           Tired of using Google Sheets
@@ -16,12 +339,4 @@ function CallToAction() {
           <br />
           with our QR code based system.
         </p>
-      </div>
-      <div className="w-1/2">
-        <img src="/confirmation.svg" alt="Confirmation" />
-      </div>
-    </div>
-  );
-}
-
-export default CallToAction;
+      </div>*/
